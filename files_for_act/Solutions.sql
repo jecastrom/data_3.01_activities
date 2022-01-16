3ceb2ac8f43d2ac5278105853f07f7fc1d2c2f56
jecastrom <jcastro.de@outlook.com>
2022-01-16 14:31:45 +0100

    a

--- files_for_act/Solutions.sql
+++ files_for_act/Solutions.sql
@@ -14,3 +14,17 @@ ORDER BY
     3 DESC
 LIMIT
     10;
+-- Are there districts with no clients?
+SELECT
+    a.district_id AS district_id,
+    b.A2 AS district_name,
+    count(a.client_id) AS number_of_clients
+FROM
+    `client` a
+    INNER JOIN district b ON a.district_id = b.A1
+GROUP BY
+    1
+HAVING
+    number_of_clients <= 1
+ORDER BY
+    3 DESC;
