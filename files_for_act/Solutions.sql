a534681ec5f781d7b6e2c0d117f481bb11674d28
jecastrom <jcastro.de@outlook.com>
2022-01-16 12:08:35 +0100

    a

---
+++ files_for_act/Solutions.sql
@@ -0,0 +1,13 @@
+-- Activity 3.01
+-- Activity 1
+-- Get the number of clients by district, returning district name.
+SELECT
+    b.A2 AS district_name,
+    count(a.client_id) AS number_of_clients
+FROM
+    `client` a
+    INNER JOIN district b ON a.district_id = b.A1
+GROUP BY
+    1
+ORDER BY
+    2 DESC;
